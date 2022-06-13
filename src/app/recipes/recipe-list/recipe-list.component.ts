7e922c724f36a8df1b512563f9f3435de35ae0d0
Bethly9996 <bethlyc@kabarak.ac.ke>
2022-06-13 21:29:17 +0300

    navigation and passing recipe data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.loveandlemons.com/wp-content/uploads/2019/09/easy-dinner-ideas-1-741x1024.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/pulled-chicken-beans-b903c2e.jpg?quality=90&webp=true&resize=750,681')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/pulled-chicken-beans-b903c2e.jpg?quality=90&webp=true&resize=750,681')
   ];
 
   constructor() { }
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+    onRecipeSelected(recipe: Recipe) {
+      this.recipeWasSelected.emit(recipe);
+    }
 }
